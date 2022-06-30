2078f8659bc78107ec222ce5d3772964527dafc6
Mikeykitili <Mikeykitili@gmail.com>
2022-06-30 15:13:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
